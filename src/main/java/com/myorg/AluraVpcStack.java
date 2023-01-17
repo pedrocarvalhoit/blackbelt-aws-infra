c4f9d8dfc8aa95e712813da2a747db750d66562f
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-01-17 09:54:00 +0000

    Vpc class

---
+++ src/main/java/com/myorg/AluraVpcStack.java
@@ -0,0 +1,28 @@
+package com.myorg;
+
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.constructs.Construct;
+
+public class AluraVpcStack extends Stack {
+
+    //Dessa maneira a vpc fica disponível para acesso no AluraAwpInfraApp
+    private Vpc vpc;
+
+    public AluraVpcStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public AluraVpcStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        vpc = Vpc.Builder.create(this, "AluraVpc")
+                .maxAzs(3)  // Default is all AZs in region
+                .build();
+    }
+
+    public Vpc getVpc() {
+        return vpc;
+    }
+}
