b45dec2155392a227bfa72acab1f5e60c7f343e1
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-01-17 09:53:22 +0000

    Example

--- src/main/java/com/myorg/AluraAwsInfraStack.java
+++ src/main/java/com/myorg/AluraAwsInfraStack.java
@@ -3,8 +3,6 @@ package com.myorg;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
 
 public class AluraAwsInfraStack extends Stack {
     public AluraAwsInfraStack(final Construct scope, final String id) {
